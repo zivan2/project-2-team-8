f12ed0013995b1ef70b3b6259e654a66508f472d
Joe Malone <joe.d.malone@gmail.com>
2021-04-10 13:29:26 -0500

    Server routes are functional, helper file added to 'views' directory

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,24 @@
-// const sequelize = require('../config/connection');
-// const { User } = require('../models');
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+const { Meds } = require('../models');
 
-// const userData = require('./userData.json');
+const userData = require('./user.json');
+const medsData = require('./meds.json');
 
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-//   await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-//   process.exit(0);
-// };
+  await Meds.bulkCreate(medsData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-// seedDatabase();
+  process.exit(0);
+};
+
+seedDatabase();
