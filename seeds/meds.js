cd8d80bae587596b46cd0ba18e8f4f08fd3f2324
Ivan Zabrodin <pyrat32@gmail.com>
2021-04-16 21:48:50 -0500

    working

--- seeds/meds.js
+++ seeds/meds.js
@@ -2,31 +2,31 @@ const { Meds } = require("../models")
 
 const medsData = [
     {
-    name: "drug1",
-    dosage: "200mg",
-    regimen: "take twice daily with food",
-    length: "14",
-    side_effects: "Drowsiness",
-    refill: "false",
-    user_id: "1"
+        name: "drug1",
+        dosage: "200mg",
+        regimen: "take twice daily with food",
+        length: "14",
+        side_effects: "Drowsiness",
+        refill: "false",
+        user_id: "1"
     },
     {
-    name: "drug2",
-    dosage: "200mg",
-    regimen: "take twice daily with food",
-    length: "15",
-    side_effects: "Drowsiness",
-    refill: "false",
-    user_id: "1"
+        name: "drug2",
+        dosage: "200mg",
+        regimen: "take twice daily with food",
+        length: "15",
+        side_effects: "Drowsiness",
+        refill: "false",
+        user_id: "1"
     },
     {
-    name: "drug3",
-    dosage: "200mg",
-    regimen: "take twice daily with food",
-    length: "16",
-    side_effects: "Drowsiness",
-    refill: "false",
-    user_id: "1"
+        name: "drug3",
+        dosage: "200mg",
+        regimen: "take twice daily with food",
+        length: "16",
+        side_effects: "Drowsiness",
+        refill: "false",
+        user_id: "1"
     }
 ]
 
